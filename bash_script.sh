39aa945b265c0410b939c40a3b78d9a18942d8cc
Aken20 <139094812+Aken20@users.noreply.github.com>
2023-12-09 23:08:22 +0400

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -4,18 +4,18 @@ echo "
           #Architecture : $(uname -a)"
 echo "          #CPU Physical : $(lscpu | grep 'Socket(s):' | awk '{print $2}')"
 echo "          #vCPU : $(lscpu | grep 'node(s):' | awk '{print $3}')"
-total_ram=$(free -m | awk '/^Mem:/ {print $2}')
-used_ram=$(free -m | awk '/^Mem:/ {print $3}')
-ram_utilization=$(free | awk '/^Mem:/ {printf("%.2f%%", $3/$2*100)}')
-echo "          #Memory Usage : $used_ram/${total_ram}MB, ($ram_utilization)"
+total_ram=$(free -m | grep 'Mem' | awk '{print $2}')
+used_ram=$(free -m | grep 'Mem' | awk '{print $3}')
+ram_utilization=$(free | grep 'Mem' | awk '{printf("%.2f%%", $3/$2*100)}')
+echo "          #Memory Usage : $used_ram/${total_ram}MB ($ram_utilization)"
 total_memory=$(df -h | awk '$NF=="/" {print $2}')
 used_memory=$(df -h | awk '$NF=="/" {print $3}')
 memory_utilization=$(df -h | awk '$NF=="/" {print $5}')
 echo "          #Disk Usage : $used_memory/$total_memory ($memory_utilization)"
-echo "          #CPU load: $(top -bn1 | grep '%Cpu(s):' | awk '{print $8}')"
+echo "          #CPU load: $(mpstat | grep 'all' | awk '{printf("%.2f%%", 100-$13)}')"
 echo "          #Last boot: $(who -b | awk '{print $3, $4}')"
 echo "          #LVM use: $(if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ]; then echo yes; else echo no; fi)"
-echo "          #Connections TCP : $(netstat -an | grep ESTABLISHED | wc -l)"
+echo "          #Connections TCP : $(netstat -an | grep ESTABLISHED | wc -l) ESTABLISHED"
 echo "          #User log : $(who | wc -l)"
 echo "          #Network : IP $(hostname -I)($(ip link | awk '/ether/ {print $2}'))"
 echo "          #Sudo : $(journalctl _COMM=sudo -q | grep COMMAND | wc -l) cmd"
