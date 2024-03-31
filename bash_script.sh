8d87d71c92fff5eb72de2eb56ea42b660b1720ea
Ahmed Ibrahim <139094812+Aken20@users.noreply.github.com>
2024-03-31 04:11:49 +0400

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-echo "
-          #Architecture : $(uname -a)"
-echo "          #CPU Physical : $(lscpu | grep 'Socket(s):' | awk '{print $2}')"
-echo "          #vCPU : $(lscpu | grep 'node(s):' | awk '{print $3}')"
 total_ram=$(free -m | grep 'Mem' | awk '{print $2}')
 used_ram=$(free -m | grep 'Mem' | awk '{print $3}')
 ram_utilization=$(free | grep 'Mem' | awk '{printf("%.2f%%", $3/$2*100)}')
-echo "          #Memory Usage : $used_ram/${total_ram}MB ($ram_utilization)"
-total_memory=$(df -h | awk '$NF=="/" {print $2}')
-used_memory=$(df -h | awk '$NF=="/" {print $3}')
-memory_utilization=$(df -h | awk '$NF=="/" {print $5}')
-echo "          #Disk Usage : $used_memory/$total_memory ($memory_utilization)"
-echo "          #CPU load: $(mpstat | grep 'all' | awk '{printf("%.2f%%", 100-$13)}')"
-echo "          #Last boot: $(who -b | awk '{print $3, $4}')"
-echo "          #LVM use: $(if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ]; then echo yes; else echo no; fi)"
-echo "          #Connections TCP : $(netstat -an | grep ESTABLISHED | wc -l) ESTABLISHED"
-echo "          #User log : $(who | wc -l)"
-echo "          #Network : IP $(hostname -I) ($(ip link | awk '/ether/ {print $2}'))"
-echo "          #Sudo : $(journalctl _COMM=sudo -q | grep COMMAND | wc -l) cmd"
+total_memory=$(df -m | grep "/dev/" | awk '{disk_t += $2} END {print disk_t}')
+used_memory=$(df -m | grep "/dev/" | awk '{disk_u += $3} END {print disk_u}')
+memory_utilization=$(printf "%d%%" "$((used_memory * 100 / total_memory))")
+wall "
+          #Architecture : $(uname -a)
+          #CPU Physical : $(lscpu | grep 'Socket(s):' | awk '{print $2}')
+          #vCPU : $(lscpu | grep 'node(s):' | awk '{print $3}')
+          #Memory Usage : $used_ram/${total_ram}MB ($ram_utilization)
+          #Disk Usage : $used_memory/$(printf "%d" "$((total_memory / 1024))")Gb ($memory_utilization)
+          #CPU load: $(mpstat | grep 'all' | awk '{printf("%.2f%%", 100-$13)}')
+          #Last boot: $(who -b | awk '{print $3, $4}')
+          #LVM use: $(if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ]; then echo yes; else echo no; fi)
+          #Connections TCP : $(netstat -an | grep ESTABLISHED | wc -l) ESTABLISHED
+          #User log : $(who | wc -l)
+          #Network : IP $(hostname -I) ($(ip link | awk '/ether/ {print $2}'))
+          #Sudo : $(journalctl _COMM=sudo -q | grep COMMAND | wc -l) cmd
+"
